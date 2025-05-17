da90b4f39c28d6bc6a9f53f33c708017bf7567c1
Alvaro Balcazar <aalvarob18@gmail.com>
2025-05-16 22:46:08 -0300

    se agregaron los delete del cartRouter

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -29,10 +29,48 @@ cartRouter.post("/:cid/product/:pid", async(req, res) => {
   try {
     const { cid, pid } = req.params;
     const { quantity } = req.body;
-    const updatedCart = await Cart.findOneAndUpdate({ _id: cid }, { $push: { products: { product: pid, quantity } } }, { new: true, runValidators: true });
+    const updatedCart = await Cart.findOneAndUpdate(
+      { _id: cid }, 
+      { $push: { products: { product: pid, quantity } } }, 
+      { new: true, runValidators: true }
+    ).populate("products.product");
     if(!updatedCart) return res.status(404).json({ status: "error", message: "Carrito no encontrado" });
 
     res.status(200).json({ status: "success", payload: updatedCart });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+cartRouter.delete("/:cid", async (req, res) => {
+  try {
+    const { cid } = req.params;
+    const updatedCart = await Cart.findOneAndUpdate(
+      { _id: cid },
+      { $set: { products: [] } },
+      { new: true }
+    );
+    if (!updatedCart) {
+      return res.status(404).json({ status: "error", message: "Carrito no encontrado" });
+    }
+    res.status(200).json({ status: "success", payload: updatedCart.products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+cartRouter.delete("/:cid/products/:pid", async (req, res) => {
+  try {
+    const { cid, pid } = req.params;
+    const updatedCart = await Cart.findOneAndUpdate(
+      { _id: cid },
+      { $pull: { products: { product: pid } } },
+      { new: true }
+    ).populate("products.product");
+    if (!updatedCart) {
+      return res.status(404).json({ status: "error", message: "Carrito no encontrado" });
+    }
+    res.status(200).json({ status: "success", payload: updatedCart.products });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
